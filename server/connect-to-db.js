d12224b165905e262b06682b0a563f5d1c5fe5b7
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2022-06-21 10:09:18 +0200

    [Server] - fix server connection call

--- server/connect-to-db.js
+++ server/connect-to-db.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-	host: 'db-base-di-dati.ckehalncburp.us-east-1.rds.amazonaws.com',
-	port: '3306',
-	user: 'admin',
-	password: 'ineedtopass3exams',
-	database: 'Pathfinder_Sheets'
-});
-
-exports.connect = () =>
+exports.connect = () => {
+	const db = mysql.createConnection({
+		host: 'db-base-di-dati.ckehalncburp.us-east-1.rds.amazonaws.com',
+		port: '3306',
+		user: 'admin',
+		password: 'ineedtopass3exams',
+		database: 'Pathfinder_Sheets'
+	});
 	db.connect((err) => {
 		if (err) {
 			console.log(err);
@@ -16,11 +15,11 @@ exports.connect = () =>
 				statusCode: 505,
 				body: JSON.stringify('Failed to connect')
 			};
-		} else {
-			console.log('Database connected');
-			return {
-				statusCode: 200,
-				body: JSON.stringify('Database connected')
-			};
 		}
+		console.log('Database connected');
+		return {
+			statusCode: 200,
+			body: JSON.stringify('Database connected')
+		};
 	});
+};
--- server/index.js
+++ server/index.js
@@ -3,10 +3,8 @@ const connectToDb = require('./connect-to-db.js');
 connectToDb.connect();
 /*
 exports.handler = async (event) => {
-	const response = connectToDb.connect();
-
-	console.log('EVENT: \n' + JSON.stringify(event, null, 2));
-
+	//console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+	const response = await connectToDb.connect();
 	return response;
 };
 */
