38314b5081eac26106a23b7a167ef98d949b5009
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2022-06-24 13:00:17 +0200

    [Client] - add get characters

--- client-react/src/home/Home.jsx
+++ client-react/src/home/Home.jsx
@@ -4,8 +4,25 @@ import { useParams } from 'react-router-dom';
 
 function Home() {
 	let params = useParams();
-
 	console.log(params.email);
+
+	const getCharactersApi = 'https://e157zbhd6c.execute-api.us-east-1.amazonaws.com/staging/access/home';
+
+	const getCharacters = () => {
+		const data = { Email: params.email };
+
+		axios
+			.post(getCharactersApi, data)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+				alert(error);
+			});
+	};
+
+	getCharacters();
 
 	return (
 		<div>
