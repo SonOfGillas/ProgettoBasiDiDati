ddb83d4171915a693ba55a1f6ad825fc9aaf3e7d
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2022-06-27 09:20:25 +0200

    [Client] - fix url api

--- client-react/src/home/Home.jsx
+++ client-react/src/home/Home.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { useState } from 'react';
+//import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Home() {
 	let params = useParams();
 	console.log(params.email);
 
-	const getCharactersApi = 'https://e157zbhd6c.execute-api.us-east-1.amazonaws.com/staging/access/home';
+	const getCharactersApi = 'https://e157zbhd6c.execute-api.us-east-1.amazonaws.com/staging/home';
 
 	const getCharacters = () => {
 		const data = { Email: params.email };
