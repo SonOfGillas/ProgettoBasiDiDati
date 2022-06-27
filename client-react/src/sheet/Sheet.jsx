d2aaf204b7b5d45c952b03ed9a88df2f0b1a7346
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2022-06-27 11:22:34 +0200

    [Client] - add  some grafic to sheet page

--- client-react/src/sheet/Sheet.jsx
+++ client-react/src/sheet/Sheet.jsx
@@ -10,59 +10,21 @@ function Sheet() {
 	let params = useParams();
 	console.log(params.CodPer);
 
-	const [ characters, setCharacters ] = useState();
-
-	const getCharactersApi = 'https://e157zbhd6c.execute-api.us-east-1.amazonaws.com/staging/home';
-
-	const getCharacters = useCallback(
-		() => {
-			const data = { Email: params.email };
-
-			axios
-				.post(getCharactersApi, data)
-				.then((response) => {
-					console.log(response);
-					setCharacters(response.data.body);
-				})
-				.catch((error) => {
-					console.log(error);
-					alert(error);
-				});
-		},
-		[ params.email ]
-	);
-
-	useEffect(
-		() => {
-			//getCharacters();
-		},
-		[ getCharacters ]
-	);
-
-	const openOcDetail = useCallback((CodPer) => {
-		navigate('/Sheet/' + CodPer);
-	});
-
-	const Character = (props) => {
-		return (
-			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 15 }}>
-				{props.character.Nome} <Button title='show' onClick={() => openOcDetail(props.character.CodPer)} />
-			</div>
-		);
-	};
-
-	const OCs = () => {
-		return (
-			<div>
-				<h1>Your Current Characters:</h1>
-				<ul>{characters.map((character, index) => <Character key={index} character={character} />)}</ul>
-			</div>
-		);
-	};
-
 	return (
 		<div style={styles.sheet} className='home'>
-			<p>Character Detail page</p>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<p>Character Detail page</p>
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
+				<div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }} />
+				<div style={{ display: 'flex', flexDirection: 'column', flexGrow: 4 }}>
+					<p>Character Detail page</p>
+				</div>
+				<div style={{ display: 'flex', flexDirection: 'column', flexGrow: 3 }}>
+					<p>Ability</p>
+				</div>
+				<div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }} />
+			</div>
 		</div>
 	);
 }
