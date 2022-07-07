1cf37b1283235ad313bad47f3d9f41b7a7c8462f
riccardo gardenghi <riccardo.gardenghi@studio.unibo.it>
2022-07-07 18:14:47 +0200

    - [ Server ]  - add delete function

---
+++ server/src/deleteFromSheet/class/deleteClass.js
@@ -0,0 +1,40 @@
+
+const mysql = require('mysql');
+
+exports.handler = async (event) => {
+	let response;
+
+	try {
+		response = new Promise((resolve) => {
+			const db = mysql.createConnection({
+				host: 'db-base-di-dati.ckehalncburp.us-east-1.rds.amazonaws.com',
+				port: '3306',
+				user: 'admin',
+				password: 'ineedtopass3exams',
+				database: 'Pathfinder_Sheets'
+			});
+
+			const query =
+              `Delete from Personaggi where CodPer=`+event['CodPer']+` NomeClasse=`+event['NomeClasse']
+			
+            db.connect();
+
+			db.query(query, (err, result) => {
+				if (err) throw err;
+				db.end();
+				resolve({
+					statusCode: 202,
+					body: result
+				});
+			});
+		});
+	} catch (e) {
+		console.log(e);
+		response = {
+			statusCode: 500,
+			body: e
+		};
+	}
+
+	return response;
+};
