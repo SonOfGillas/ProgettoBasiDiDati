18f756486603dabba4c71e5fb55b05702af096a9
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2022-07-10 09:37:06 +0200

    [ server ] -  fix addOC api

--- server/src/addToSheet/addCharacter.js
+++ server/src/addToSheet/addCharacter.js
@@ -1,4 +1,3 @@
-
 const mysql = require('mysql');
 
 exports.handler = async (event) => {
@@ -15,58 +14,29 @@ exports.handler = async (event) => {
 			});
 
 			const query =
-              `INSERT INTO Classi (
-                CodPer,
-                Nome,
-                EmailUser,
-                Razza,
-                Allineamento,
-                HP,
-                CA,
-                BAB,
-                BMC,
-                DMC,
-                Tempra,
-                Riflessi,
-                Volonta,
-                Forza,
-                Destrezza,
-                Costituzione,
-                Intelligenza,
-                Saggezza,
-                Carisma,
-                CompArmature,
-                CompScudi,
-                CompArmiGuerra,
-                CompeArmiEsotiche,
-                ) VALUES (
-                `+event['CodPer']+`,
-                '`+event['Nome']+`',
-                '`+event['EmailUser']+`',
-                '`+event['Razza']+`',
-                '`+event['Allineamento']+`',
-                `+event['HP']+`,
-                `+event['CA']+`,
-                `+event['BAB']+`,
-                `+event['BMC']+`,
-                `+event['DMC']+`,
-                `+event['Tempra']+`,
-                `+event['Riflessi']+`,
-                `+event['Volonta']+`,
-                `+event['Forza']+`,
-                `+event['Destrezza']+`,
-                `+event['Costituzione']+`,
-                `+event['Intelligenza']+`,
-                `+event['Saggezza']+`,
-                `+event['Carisma']+`,
-                `+event['CompArmature']+`,
-                `+event['CompScudi']+`,
-                `+event['CompArmiGuerra']+`,
-                `+event['CompeArmiEsotiche']+`,
-                );`;
-            
-			
-            db.connect();
+				"INSERT INTO Personaggi (Nome,EmailUser,Razza,Allineamento,Forza,Destrezza,Costituzione,Intelligenza,Saggezza,Carisma) VALUES ('" +
+				event['Nome'] +
+				"','" +
+				event['EmailUser'] +
+				"','" +
+				event['Razza'] +
+				"','" +
+				event['Allineamento'] +
+				"'," +
+				event['Forza'] +
+				',' +
+				event['Destrezza'] +
+				',' +
+				event['Costituzione'] +
+				',' +
+				event['Intelligenza'] +
+				',' +
+				event['Saggezza'] +
+				',' +
+				event['Carisma'] +
+				')';
+
+			db.connect();
 
 			db.query(query, (err, result) => {
 				if (err) throw err;
