4f17947a01f00f1f295975c7c2847a8582b3b57b
riccardo gardenghi <riccardo.gardenghi@studio.unibo.it>
2022-07-20 18:50:04 +0200

    - [ Server ]  - update createCaracter

--- server/src/addToSheet/addCharacter.js
+++ server/src/addToSheet/addCharacter.js
@@ -13,7 +13,14 @@ exports.handler = async (event) => {
 				database: 'Pathfinder_Sheets'
 			});
 
-			const query =
+			const getRaceCharateristicsMod = "Select ModForza,ModDestrezza,ModCostituzione,ModIntelligenza,ModSaggezza,ModCarisma from Razze where Nome='"+event['Razza']+"'";
+			
+			db.connect();
+
+			db.query(getRaceCharateristicsMod, (err, raceCharateristicsMod) => {
+				if (err) throw err;
+
+				const query =
 				"INSERT INTO Personaggi (Nome,EmailUser,Razza,Allineamento,Forza,Destrezza,Costituzione,Intelligenza,Saggezza,Carisma) VALUES ('" +
 				event['Nome'] +
 				"','" +
@@ -23,29 +30,29 @@ exports.handler = async (event) => {
 				"','" +
 				event['Allineamento'] +
 				"'," +
-				event['Forza'] +
+				(parseInt(event['Forza'])+parseInt(raceCharateristicsMod[0].ModForza)) +
 				',' +
-				event['Destrezza'] +
+				(parseInt(event['Destrezza'])+parseInt(raceCharateristicsMod[0].ModDestrezza)) +
 				',' +
-				event['Costituzione'] +
+				(parseInt(event['Costituzione'])+parseInt(raceCharateristicsMod[0].ModCostituzione)) +
 				',' +
-				event['Intelligenza'] +
+				(parseInt(event['Intelligenza'])+parseInt(raceCharateristicsMod[0].ModIntelligenza)) +
 				',' +
-				event['Saggezza'] +
+				(parseInt(event['Saggezza'])+parseInt(raceCharateristicsMod[0].ModSaggezza)) +
 				',' +
-				event['Carisma'] +
+				(parseInt(event['Carisma'])+parseInt(raceCharateristicsMod[0].ModCarisma)) +
 				')';
 
-			db.connect();
-
-			db.query(query, (err, result) => {
-				if (err) throw err;
-				db.end();
-				resolve({
-					statusCode: 202,
-					body: result
+				db.query(query, (err, result) => {
+					if (err) throw err;
+					db.end();
+					resolve({
+						statusCode: 202,
+						body: result
+					});
 				});
 			});
+
 		});
 	} catch (e) {
 		console.log(e);
